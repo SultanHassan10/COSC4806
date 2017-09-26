b307e4a0b4000ae6bcd7ae85df309e631a0a2e51
SultanHassan10 <32252729+SultanHassan10@users.noreply.github.com>
2017-09-25 22:44:12 -0400

    array and loops
    this php file includes the arrays of the possible usernames and passwords and decide whether they are right or wrong and displays the proper message for the user.

---
+++ welcome.php
@@ -0,0 +1,26 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$names = array("Sultan","Salman","Faisal","Mazin");
+$pass = array("1111", "2222", "3333", "4444");
+$NArrlength = sizeof($names);
+$PArrlength = count($pass);
+
+//for($i = 0; $i < $PArrlength; $i++) {
+	for($j = 0; $j < $NArrlength; $j++) {
+
+	if(($username == $names[$j] AND $password == $pass[$j]) == TRUE){
+	echo "Hello $username, you have successfully logged in!";
+	
+}//if statement
+
+	elseif(/*($username == $names[$j] AND $password == $pass[$j]) == */FALSE){
+	echo "password or username is wrong!!!";
+	
+}//else if statement
+}//second for loop
+//}//first for loop
+
+?>
+
+
