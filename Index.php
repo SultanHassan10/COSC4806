21606c22fd107ba5dfee04ba1d26fe37ab383fa7
SultanHassan10 <32252729+SultanHassan10@users.noreply.github.com>
2017-09-25 22:40:56 -0400

    log in screen
    here is the code that displays the header and the submission button in the log in screen

---
+++ Index.php
@@ -0,0 +1,9 @@
+<h1> Hello There </h1>
+<P1> Please enter your username and password <P1>
+<form action="welcome.php" method="POST">
+<br>username: <input type="text" name="username"/></br>
+password: <input type="password" name="password"/>
+<input type="submit" value="submit"/>
+
+</form>
+
