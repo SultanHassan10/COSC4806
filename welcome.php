02a44d7b825ad14018fa12884821cfcf41e003d3
SultanHassan10 <32252729+SultanHassan10@users.noreply.github.com>
2017-10-02 00:56:18 -0400

    changes weren't right

--- welcome.php
+++ welcome.php
@@ -1,22 +1,20 @@
 <?php
 $username = $_POST['username'];
 $password = $_POST['password'];
-$names = array("Sultan"=>"1111","Salman"=>"2222","Faisal"=>"3333","Mazin"=>"4444");
-//$pass = array("1111", "2222", "3333", "4444");
+$names = array("Sultan","Salman","Faisal","Mazin");
+$pass = array("1111", "2222", "3333", "4444");
 $NArrlength = count($names);
-//$PArrlength = count($pass);
+$PArrlength = count($pass);
 
 //for($i = 0; $i < $PArrlength; $i++) {
 	for($j = 0; $j < $NArrlength; $j++) {
 
-//	if(($username == $names[$j] AND $password == $pass[$j]) == TRUE){
-	if(($username == $names[$j]) == TRUE){
+	if(($username == $names[$j] AND $password == $pass[$j]) == TRUE){
 	echo "Hello $username, you have successfully logged in!";
 	
 }//if statement
 
-//	elseif(($username == $names[$j] AND $password == $pass[$j]) == FALSE){
-	else{
+	elseif(/*($username == $names[$j] AND $password == $pass[$j]) ==*/ FALSE){
 	echo "password or username is wrong!!!";
 	
 }//else if statement
