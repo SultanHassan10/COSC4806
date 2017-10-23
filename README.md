82ba19dd0981d64f58d1328ac8c35d0adedb296c
SultanHassan10 <32252729+SultanHassan10@users.noreply.github.com>
2017-10-22 21:57:13 -0400

    MVC
    i added the MVC code folder to the xampp htdocs file

--- README.md
+++
@@ -1,7 +0,0 @@
-# Cosc4806
-
-# Sultan Hassan
-# assignment 1
-
-
-# https://github.com/SultanHassan10/Cosc4806
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-SESSION_start();
-SESSION_destroy();
-$_SESSION['isauthenticated'] = false;
-header("Location: index.php");
-
-?>
