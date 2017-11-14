930fbcec1faa310bf5481a56d375b1c8a7d897be
SultanHassan10 <32252729+SultanHassan10@users.noreply.github.com>
2017-11-14 00:06:05 -0500

    footer creation

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,14 +1,31 @@
 <footer class="footer">    
     <div class="row">
         <div class="col-lg-12">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
+            
         </div>
     </div>
 </footer>
 
+<html>
+<head>
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: purple;
+   color: white;
+   text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="footer">
+  <p>Copyright &copy; <?php echo date('Y'); ?> </p>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
 </body>
+</html> </body>
 </html>
